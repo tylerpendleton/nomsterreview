4f715bcf6000278e3e77a50826dd535031a819f3
tylerpendleton <tyler@tylerpendleton.com>
2017-08-08 14:18:09 -0700

    added nonfunctional form and some gems

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'will_paginate', '~> 3.1.0'
+gem 'simple_form'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,23 @@ class PlacesController < ApplicationController
   def index
     @places = Place.paginate(:page => params[:page], :per_page => 5) 
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.create(place_params[:params])
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+  def place_params
+  end
 end
